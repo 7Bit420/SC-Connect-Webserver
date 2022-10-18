1b2929732776f942a43b875aaad8298833c9bfd4
7Bit <87518152+7Bit420@users.noreply.github.com>
2022-10-19 08:20:19 +1100

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import { serverListner } from './serverListner'
 import * as http from 'http'
 
+interface handler {
+    path: string,
+    special: boolean
+    handler: (
+        req: http.IncomingMessage,
+        res: http.ClientRequest
+    ) => void
+}
+
+const handlers: handler[] = []
+
 const server = http.createServer({},serverListner)
 
 server.listen(80)
 
+export { server, handlers }
--- src/serverListner.ts
+++ src/serverListner.ts
@@ -1,12 +1,15 @@
 import * as http from 'http'
+import { handlers } from '.';
 
 function serverListner(
     req: http.IncomingMessage,
     res: http.ServerResponse
 ) {
+    const reqUrl = new URL(req.url, `http://${req.headers.host}`);
     
+
 }
 
 
 
-export { serverListner }
+export { serverListner }
